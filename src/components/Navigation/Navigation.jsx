038aa861886b2b48cada227a4f3c3dbf171c1178
seanvaldez <rhinecedrick@gmail.com>
2023-01-02 23:13:26 +0800

    updates

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="py-5 shadow-xl border-b flex bg-white z-10 sticky top-0">
+    <nav className="py-5 shadow-xl border-b flex bg-white z-10 sticky top-0 px-4 sm:px-0">
       <div className="container flex items-center justify-between w-full">
         <p className="font-bold">Logo</p>
         <div className="flex items-center gap-x-4">
